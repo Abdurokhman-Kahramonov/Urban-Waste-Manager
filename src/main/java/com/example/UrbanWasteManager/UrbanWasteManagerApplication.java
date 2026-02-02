23e5715fba5525846ff9e2e4931eda0f4717a172
Abdurohman Kahramonov <abdurohman.kahramonov@servpoint.org>
2026-02-03 01:16:06 +0500

    last update

--- src/main/java/com/example/UrbanWasteManager/UrbanWasteManagerApplication.java
+++ src/main/java/com/example/UrbanWasteManager/UrbanWasteManagerApplication.java
@@ -6,7 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class UrbanWasteManagerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(UrbanWasteManagerApplication.class, args);
-	}
-}
+	        public static void main(String[] args) {
+
+	            SpringApplication.run(UrbanWasteManagerApplication.class, args);
+
+	        }}
